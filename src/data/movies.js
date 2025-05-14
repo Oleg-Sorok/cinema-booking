084bed460c75fc87be201137e8351e02876e4df3
Oleg-Sorok <olegsorokevych@gmail.com>
2025-05-14 23:04:03 +0300

    Додано дані для фільмів

---
+++ src/data/movies.js
@@ -0,0 +1,52 @@
+const movies = [
+  {
+    id: '1',
+    title: 'Інтерстеллар',
+    genre: 'Наукова фантастика',
+    description: 'Подорож у часі та просторі.',
+    time: '2025-05-15 18:30',
+    image: '/images/interstellar.jpg',
+  },
+  {
+    id: '2',
+    title: 'Темний лицар',
+    genre: 'Бойовик',
+    description: 'Бетмен протистоїть Джокеру в епічному протистоянні.',
+    time: '2025-05-15 21:00',
+    image: '/images/dark_knight.jpg',
+  },
+  {
+    id: '3',
+    title: 'Початок',
+    genre: 'Трилер, Фантастика',
+    description: 'Група спеціалістів проникає у сни, щоб змінити підсвідомість.',
+    time: '2025-05-16 19:00',
+    image: '/images/inception.jpg',
+  },
+  {
+    id: '4',
+    title: 'Зелена Миля',
+    genre: 'Драма, Фентезі',
+    description: 'Незвичайна історія у в’язниці смертників.',
+    time: '2025-05-16 21:30',
+    image: '/images/green_mile.jpg',
+  },
+  {
+    id: '5',
+    title: 'Матриця',
+    genre: 'Фантастика, Бойовик',
+    description: 'Хакер дізнається правду про реальність.',
+    time: '2025-05-17 20:00',
+    image: '/images/matrix.jpg',
+  },
+  {
+    id: '6',
+    title: 'Втеча з Шоушенка',
+    genre: 'Драма',
+    description: 'Історія надії, дружби і визволення.',
+    time: '2025-05-17 22:30',
+    image: '/images/shawshank.jpg',
+  },
+];
+
+export default movies;
